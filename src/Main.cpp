4c0ba3b191bc25047141c55f5fae0fa9d9879534
ruben.romani.fedora <ruben.romani@outlook.it>
2024-05-01 20:15:35 +0200

    data reader function in progress

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "DataReader.hpp"
+#include "BarTypes.hpp"
 
 int main(){
     std::cout << "Hello from BacktestingEngine" << std::endl; 
+    auto dataReader = DataReader<Bar>();
+    auto priceSeries = dataReader.readFromFile("../data/AUDUSD_M1.csv");
+    printf("Size %d", priceSeries.size());
+    for( auto bar : priceSeries.series()){
+        printf("%ld\t%f\t%f\t%f\t%f\n",
+        bar.time,bar.open,bar.high,bar.low,bar.close);
+    }
     return 0;
 }
