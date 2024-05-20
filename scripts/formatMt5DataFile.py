961f2c6beee87dc3a3915daeef87f58fd10c95a8
ruben.romani.fedora <ruben.romani@outlook.it>
2024-05-20 11:30:28 +0200

    python script is able to format more symbols and timeframes

--- scripts/formatMt5DataFile.py
+++ scripts/formatMt5DataFile.py
@@ -2,9 +2,9 @@ import os
 import datetime
 import time
 
-def main():
-    in_path = '../data/AUDUSD_M1.csv'
-    out_path = '../data/AUDUSD_M1_pars.csv'
+def parse(inpath, outpath):
+    in_path = inpath
+    out_path = outpath
     data=''
     with open(in_path) as f:
         data = f.read()
@@ -16,11 +16,44 @@ def main():
             continue
         nline+=1
         tmp = line.split('\t')
-        tmp[1] = str(int(time.mktime(datetime.datetime.strptime(tmp[0].strip() + " " + tmp[1].strip(),"%Y.%m.%d %H:%M:%S").timetuple())))
+        try:
+            tmp[1] = str(int(time.mktime(datetime.datetime.strptime(tmp[0].strip() + " " + tmp[1].strip(),"%Y.%m.%d %H:%M:%S").timetuple())))
+        except:
+            tmp[1] = str(int(time.mktime(datetime.datetime.strptime(tmp[0].strip(),"%Y.%m.%d").timetuple())))
         tmp = tmp[1:]
         result.append(','.join(tmp))
     with open(out_path, "w") as text_file:
         text_file.write('\n'.join(result))
+
+def main():
+    timeframes = [
+        "M1",
+        "M5",
+        "M10",
+        "M15",
+        "M30",
+        "H1",
+        "H4",
+        "Daily",
+        "Weekly",
+        "Monthly"
+    ]
+
+    symbols = [
+        "AUDUSD",
+        "EURUSD",
+        "GBPUSD",
+        "USDCAD",
+        "USDCHF",
+        "USDJPY",
+    ]
+
+    for timeframe in timeframes:
+        for symbol in symbols:
+            inpath = '../data/'+ symbol + '_' + timeframe + '.csv'
+            outpath = '../data/'+ symbol + '_' + timeframe + '_pars.csv'
+            parse(inpath,outpath)
+
 
 if __name__=="__main__":
     main()
